3c72abc291c9642944a9ff1764d88397c930a6b3
ahmedfoyez786 <ahmedfoyez786@gmail.com>
2023-03-08 11:38:02 +0600

    edit templates

--- calculator.py
+++ calculator.py
@@ -1,35 +1,25 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/cal/<int:num1>/<op>/<int:num2>')
-def calculator(num1, op, num2):
-    if op == '+':
-        result = num1 + num2
-    elif op == '-':
-        result = num1 - num2
-    elif op == '*':
-        result = num1 * num2
-    elif op == '/':
-        result = num1 / num2
-    elif op == '%':
-        result = num1 % num2
-    else:
-        return "Invalid operator"
-
-    return f"{num1} {op} {num2} = {result}"
-
-
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/cal/<int:num1>/<op>/<int:num2>')
+def calculate(num1, op, num2):
+    if op == 'add':
+        result = num1 + num2
+    elif op == 'subtract':
+        result = num1 - num2
+    elif op == 'multiply':
+        result = num1 * num2
+    elif op == 'modulus':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
 
-
+    return render_template('user.html', num1=num1, op=op, num2=num2, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
